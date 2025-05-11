e81feab82d2eb2300e5e8df708e5226f8b5a07aa
Jenny Chen <yizhenchen06@gmail.com>
2025-05-12 01:15:24 +0800

    20250521 fix route

--- src/main.jsx
+++ src/main.jsx
@@ -66,6 +66,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
-    <RouterProvider router={router}  />
+    <RouterProvider router={router} basename="/portfolio" />
   </React.StrictMode>,
 )
